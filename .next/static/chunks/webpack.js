a174c47781c921c95da5810b601359d99ad26dda
PabloR9080 <a19216279@alumnos.uady.mx>
2023-03-23 08:57:03 -0600

    generic commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "be2bb08859fde5ef"; }
+/******/ 		__webpack_require__.h = function() { return "242b24877011fa68"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
