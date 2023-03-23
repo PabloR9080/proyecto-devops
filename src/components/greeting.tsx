3b7c917ba20a7a81e546dcc4932c0cadc5069de6
PabloR9080 <a19216279@alumnos.uady.mx>
2023-03-23 09:17:07 -0600

    feat(tailwind): delete .next and add tailwindcss

--- src/components/greeting.tsx
+++ src/components/greeting.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/styles.module.css'
 export default function Greeting({title}){
     return (
         <>
-            <h1 className={styles.title}>{title ? title : "A cool title here!"}</h1>
+            <h1 className={styles.title +" text-3xl font-bold underline"}>{title ? title : "A cool title here!"}</h1>
         </>
     )
 } 
