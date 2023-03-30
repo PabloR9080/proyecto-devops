2d9930d750b6f5524338392cb826e125d660f6cd
CarlosMoralesR <carlangetas007@hotmail.com>
2023-03-29 22:37:55 -0600

    fix(api): refactoring get and post methods in handler using switch

--- src/pages/api/cards/index.ts
+++ src/pages/api/cards/index.ts
@@ -5,28 +5,27 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // Create Card
-  if (req.method === "POST") {
-    const { type, bankName, number, expiryDate, accountId } = req.body;
+  const { method } = req;
 
-    const newCard = await db.card.create({
-      data: {
-        type,
-        bankName,
-        number,
-        expiryDate,
-        accountId: null,
-      },
-    });
-
-    res.status(201).json(newCard);
-  }
-  // Get all cards
-  else if (req.method === "GET") {
-    const cards = await db.card.findMany();
-
-    res.json(cards);
-  } else {
-    res.status(405).json({ message: "Method not allowed" });
+  switch (method) {
+    case "GET":
+      const cards = await db.card.findMany();
+      res.json(cards);
+      break;
+    case "POST":
+      const { type, bankName, number, expiryDate, accountId } = req.body;
+      const newCard = await db.card.create({
+        data: {
+          type,
+          bankName,
+          number,
+          expiryDate,
+          accountId: null,
+        },
+      });
+      res.status(201).json(newCard);
+      break;
+    default:
+      res.status(405).json({ message: "Method not allowed" });
   }
 }
