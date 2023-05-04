0e59408b7baf137fa2e505a9b1acaa5cb2fc6042
Ednover <ed_leo1411@hotmail.com>
2023-05-03 20:28:13 -0600

    fix(api): add status 500 to get and post

--- src/pages/api/cards/index.ts
+++ src/pages/api/cards/index.ts
@@ -10,22 +10,30 @@ export default async function handler(
   switch (method) {
     // Get all cards
     case "GET":
-      const cards = await db.card.findMany();
-      res.json(cards);
+      try {
+        const cards = await db.card.findMany();
+        res.json(cards);
+      } catch (error) {
+        res.status(500).json({ message: "Error retrieving cards" });
+      }
       break;
     // Create card
     case "POST":
-      const { type, bankName, number, expiryDate, accountId } = req.body;
-      const newCard = await db.card.create({
-        data: {
-          type,
-          bankName,
-          number,
-          expiryDate,
-          accountId: null,
-        },
-      });
-      res.status(201).json(newCard);
+      try {
+        const { type, bankName, number, expiryDate, accountId } = req.body;
+        const newCard = await db.card.create({
+          data: {
+            type,
+            bankName,
+            number,
+            expiryDate,
+            accountId: null,
+          },
+        });
+        res.status(201).json(newCard);
+      } catch (error) {
+        res.status(500).json({ message: "Error creating card" });
+      }
       break;
     default:
       res.status(405).json({ message: "Method not allowed" });
