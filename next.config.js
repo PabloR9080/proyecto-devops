8230fc8078a274ad3e531576d9ebee62ca58b722
PabloR9080 <a19216279@alumnos.uady.mx>
2023-05-22 15:19:18 -0600

    feat(next.config): remove unecessarily settings

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextLogger = require('next-logger');
 const nextConfig = {
     /* config options here */
-    compiler:{
-      removeConsole: true,
-    }
   };
    
 module.exports = nextConfig;
